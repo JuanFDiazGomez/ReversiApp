ca07b719658b34c3f135951b1784018c0ff89552
Juan Francisco Diaz Gomez <juanfdg1987@gmail. com>
2016-10-14 01:39:24 +0200

    Preparando la pantalla puntuaciones

---
+++ app/src/main/java/com/quantumdeers/reversiw/Puntuaciones.java
@@ -0,0 +1,13 @@
+package com.quantumdeers.reversiw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Puntuaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puntuaciones);
+    }
+}
